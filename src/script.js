80aea6fca7070f8746392772a8863d6ce9f10711
Denis Glebov <dengleboff@yandex.ru>
2024-04-08 16:10:03 +0500

    пишу на JS

---
+++ src/script.js
@@ -0,0 +1,17 @@
+import { Grid } from './grid.js'
+import { Tile } from './tile.js'
+const gameBoard = document.getElementById('game-board')
+
+const grid = new Grid(gameBoard)
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
+setupInputOnce()
+
+function setupInputOnce() {
+	window.addEventListener('keydown', handleInput, { once: true })
+}
+
+function handleInput(event) {
+	console.log(event.key)
+	setupInputOnce()
+}
